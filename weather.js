095e11ac65d66ed82d5c06e6772b7d4513bf3706
Anisha Das <74406679+Anisha-012@users.noreply.github.com>
2023-02-05 20:11:01 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -5,28 +5,28 @@ let tempicon = document.querySelector("#tempicon");
 let weatherCountry = document.querySelector("#weatherCountry");
 let weatherDescription = document.querySelector("#weatherDescription");
 let temperature = document.querySelector("#temperature");
-let feelsLike = document.querySelector("#feelslike");
-let Humidity = document.querySelector("#humidity");
-let longitude = document.querySelector("longitude");
+let feelsLike = document.querySelector("#feelsLike");
+let Humidity = document.querySelector("#Humidity");
+let longitude = document.querySelector("#longitude");
 let latitude = document.querySelector("#latitude");
 check.addEventListener("click",()=>{
    // console.log(country.value);
     //console.log(city.value);
-    let key='d1254a67722ab3cd736999bb8fcea671'
-    let url='https://api.openweathermap.org/data/2.5/weather?q=${city.value},${country.value}&appid=${key}';
+    let key=`d1254a67722ab3cd736999bb8fcea671`
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${city.value},${country.value}&appid=${key}`;
     fetch(url).then(response =>{
         return response.json();
     }).then(data=>{
         console.log(data);
-        weatherCountry.Country.innerText='${data.name} / ${data.sys.country}';
-        temperature.innerHTML = '${data.main.temp}';
+        weatherCountry.innerText=`${data.name} / ${data.sys.country}`;
+        temperature.innerHTML = `${data.main.temp}`;
         data.weather.forEach(items =>{
             weatherDescription.innerText = items.description;
         })
-        feelsLike.innerText = 'Feels like ${data.main.feels_like}';
-        Humidity.innerText =' Humidity ${data.humidty}';
-        longitude.innerText = 'Longitude ${data.coords.lon}';
-        latitude.innerText = 'Latitude ${data.coords.lat}';
+        feelsLike.innerText = `Feels like ${data.main.feels_like}`;
+        Humidity.innerText =`Humidity ${data.main.humidity}`;
+        longitude.innerText = `Longitude ${data.coord.lon}`;
+        latitude.innerText = `Latitude ${data.coord.lat}`;
     })
     country.value="";
     city.value="";
